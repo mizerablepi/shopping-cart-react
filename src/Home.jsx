801a638770c0277958d34aed2030dabc06872d82
miz <mizerablepi@gmail.com>
2023-08-28 15:56:45 +0530

    fix width issue

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,10 @@
 import HomeItemCard from "./HomeItemCard";
-// import jewelryImage from "/jewelry.jpg";
 
 function Home() {
   return (
     <div>
-      <div className="bg-[url(/hero.jpg)] h-[90vh] bg-cover flex items-center justify-end min-w-[79rem]">
-        <div className="mr-20 flex flex-col max-w-2xl flex-1">
+      <div className="bg-[url(/hero.jpg)] h-[90vh] bg-cover flex items-center justify-end">
+        <div className="ml-[37rem] mr-20 flex flex-col max-w-2xl flex-1">
           <span className="pb-2 text-lg">Winter Fashion</span>
           <span className="text-6xl font-bold pb-10">
             Fashion Collection
@@ -17,8 +16,8 @@ function Home() {
         </div>
       </div>
 
-      <div className="flex">
-        <HomeItemCard img={"/electronics.jpg"} />
+      <div className="grid grid-cols-[repeat(auto-fill,minmax(20rem,1fr))] gap-4 mt-4">
+        <HomeItemCard img={"/electronics.jpg"} title={"Visit electronics"} />
         <HomeItemCard img={"/women-wear.jpg"} />
         <HomeItemCard img={"/jewelry.jpg"} />
       </div>
--- src/HomeItemCard.jsx
+++ src/HomeItemCard.jsx
@@ -1,11 +1,10 @@
 export default function HomeItemCard({ img, title }) {
-  return <div className={`bg-[url(${img})] bg-cover h-96`}>{title}</div>;
-  // return (
-  //   <div className="relative h-[20rem] w-[10rem]">
-  //     <img src={img} className="absolute object-cover" />
-  //     <span className="absolute bottom-24">
-  //       This is text related to the product yaay
-  //     </span>
-  //   </div>
-  // );
+  return (
+    <div
+      style={{ backgroundImage: `url(${img})` }}
+      className={`bg-cover h-[40rem]`}
+    >
+      {title}
+    </div>
+  );
 }
