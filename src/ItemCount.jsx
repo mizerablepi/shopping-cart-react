bf5136b2bde74d767a07797064faa1297303be35
miz <mizerablepi@gmail.com>
2023-08-29 23:28:11 +0530

    fix bug with tailwind

--- src/ItemCount.jsx
+++ src/ItemCount.jsx
@@ -1,12 +1,12 @@
 export default function ItemCount({ count }) {
   if (count < 10) {
     return (
-      <a
-        href=""
-        className={`after:content-['${count.toString()}'] after:absolute after:top-[-0.8rem] after:left-6`}
-      >
-        <img src="/cart-outline.svg" alt="checkout" className="h-6" />
-      </a>
+      <>
+        <a href="" className={``}>
+          <img src="/cart-outline.svg" alt="checkout" className="h-6" />
+        </a>
+        <div className="absolute top-[-0.8rem] left-6">{count.toString()}</div>
+      </>
     );
   } else {
     return (
