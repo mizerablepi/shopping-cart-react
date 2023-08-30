e5d4b88f63c7eba22614538075bf4ee47c09ae37
miz <mizerablepi@gmail.com>
2023-08-30 14:31:11 +0530

    update cart route

--- src/Route.jsx
+++ src/Route.jsx
@@ -2,6 +2,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./Home";
 import Shop from "./Shop";
 import Root from "./Root";
+import Cart from "./Cart";
 
 export default function Route() {
   const router = createBrowserRouter([
@@ -21,6 +22,7 @@ export default function Route() {
           path: "/shop/:category",
           element: <Shop />,
         },
+        { path: "/cart", element: <Cart /> },
       ],
     },
   ]);
